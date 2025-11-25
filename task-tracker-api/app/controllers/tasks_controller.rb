b884f62857464ddd589ec780499529cdf0d2a2af
Lina Soler <lipasofra.ph@gmail.com>
2025-11-24 21:54:15 -0500

    added details to endoints

--- task-tracker-api/app/controllers/tasks_controller.rb
+++ task-tracker-api/app/controllers/tasks_controller.rb
@@ -1,12 +1,18 @@
 class TasksController < ApplicationController
   def index
     tasks = Task.all.order(created_at: :desc)
-    render json: tasks
+    render json: tasks, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 
   def create
-    task = Task.create(task_params)
-    render json: task
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: {errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
